d777f9c0d58761e61b9cabf9d82773aed400a631
Maryam <Maryam@maryams-mbp-2.lan>
2019-06-19 14:20:35 -0400

    added the implementation of Binary Search Tree

---
+++ BinarySearchTree.py
@@ -0,0 +1,93 @@
+
+#   BinarySearchTree.py
+#   PythonAlgorithms
+#
+#   Created by Maryam Ashoori on June 2019.
+
+class Node:
+    def __init__(self, x):
+        self.value = x
+        self.right = None
+        self.left = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # Insertion
+    def insert(self, value):
+        if (self.root == None):
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, curr_node, value):
+        if (value <curr_node.value):
+            if curr_node.left == None:
+                curr_node.left = Node(value)
+                return
+            else:
+                self._insert(curr_node.left , value)
+        elif (value > curr_node.value ):
+            if curr_node.right == None:
+                curr_node.right = Node(value)
+            else:
+                self._insert(curr_node.right, value)
+        else:
+            print ("item already in the tree")
+
+    # Traversal
+    def print(self):
+        if self.root != None:
+            self._print (self.root)
+
+    def _print(self, curr_node):
+        if curr_node == None:
+            return
+        self._print (curr_node.left)
+        print (curr_node.value)
+        self._print(curr_node.right)
+
+    # Height
+    def height (self):
+        if self.root != None:
+            return self._height (self.root, 0)
+        else:
+            return 0
+
+    def _height (self, curr_node, height):
+        if curr_node != None:
+            return max (self._height (curr_node.left, height), self._height(curr_node.right, height))+1
+        else:
+            return height
+
+    # Check membership
+    def findTarget(self, value):
+        if self.root != None:
+            return self._findTarget (self.root, value)
+        else:
+            return False
+
+    def _findTarget (self, curr_node, target):
+        if curr_node != None:
+            if target == curr_node.value:
+                return True
+            elif (target > curr_node.value):
+                return self._findTarget(curr_node.right, target)
+            else:
+                return self._findTarget(curr_node.left, target)
+        else:
+            return False
+
+# Testing the class
+tree = BinarySearchTree ()
+tree.insert(10)
+tree.insert(5)
+tree.insert(6)
+tree.insert(4)
+tree.insert(11)
+tree.print()
+print ("Height =" , tree.height())
+print ("Target found = ", tree.findTarget(15))
+
+
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# pythonAlgorithms
-Python implementations of common computer science data structures &amp; algorithms
+# PythonAlgorithms
+
+Python implementations of common computer science data structures & algorithms.
+
+## Data Structures 
+- [Binary Search Tree](BinarySearchTree.py)
+
+## Wishlist
+
+- [Splay Trees](https://en.wikipedia.org/wiki/Splay_tree)
+- [Djikstra's Algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+- [Kruskal's Algorithm](https://en.wikipedia.org/wiki/Kruskal%27s_algorithm)
+- [A* Search](https://en.wikipedia.org/wiki/A*_search_algorithm)
+- [Alpha-Beta Pruning](https://en.wikipedia.org/wiki/Alpha–beta_pruning)
+- [Bellman-Ford](https://en.wikipedia.org/wiki/Bellman–Ford_algorithm)
+- [Floyd-Warshall](https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm)
+- [Maze Generation](https://en.wikipedia.org/wiki/Maze_generation_algorithm)
+- [Red-Black Tree](https://en.wikipedia.org/wiki/Red–black_tree)
+- [Heap](https://en.wikipedia.org/wiki/Heap_(data_structure))
+- [Priority Queue](https://en.wikipedia.org/wiki/Priority_queue)
+- [Trie](https://en.wikipedia.org/wiki/Trie)
+- [Union Find](https://en.wikipedia.org/wiki/Disjoint-set_data_structure)
+- [Graph](https://en.wikipedia.org/wiki/Graph_(abstract_data_type))
+- Breadth First Search [[description](https://en.wikipedia.org/wiki/Breadth-first_search)]
+- [Depth First Search](https://en.wikipedia.org/wiki/Depth-first_search)
+- [Prim's Minimum Spanning Tree](https://en.wikipedia.org/wiki/Prim%27s_algorithm)
+- [Fisher-Yates](https://en.wikipedia.org/wiki/Fisher–Yates_shuffle)
+- [Insertion Sort](https://en.wikipedia.org/wiki/Insertion_sort)
+- [Merge Sort](https://en.wikipedia.org/wiki/Merge_sort)
+- [Heap Sort](https://en.wikipedia.org/wiki/Heapsort)
+- [Boyer-Moore](https://en.wikipedia.org/wiki/Boyer–Moore_string_search_algorithm)
+- [Knuth-Morris-Pratt](https://en.wikipedia.org/wiki/Knuth–Morris–Pratt_algorithm)
+
+# License
+This project uses the [Apache License Version 2.0](LICENSE) software license.
