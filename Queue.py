4d6acf74f6ef2dc59acbd5fdf33f8f3e33f55157
Maryam <Maryam@maryams-mbp-2.lan>
2019-06-19 19:02:45 -0400

    added more comments

--- Queue.py
+++ Queue.py
@@ -1,3 +1,8 @@
+#   BinarySearchTree.py
+#   PythonAlgorithms
+#
+#   Created by Maryam Ashoori on June 2019.
+
 class Queue:
     def __init__(self):
         self.queue = []
@@ -9,6 +14,10 @@ class Queue:
     def print(self):
         for item in self.queue:
             print(item)
+
+    # A different implementation of the print function
+    def __repr__(self):
+        return ",".join(map(lambda x: str(x), self.queue))
 
     # Returns the front of the queue
     def front(self):
@@ -25,5 +34,6 @@ if __name__ == "__main__":
     q.push (5)
     q.push (10)
     q.print()
+    print("queue:", q) #calling __repr__
     print ("The item at the front of the queue is", q.front())
     print (q.pop(), "is popped")
