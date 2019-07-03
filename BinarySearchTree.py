717d592327915e83762555282754ba1e30257f37
Maryam Ashoori <maryam@us.ibm.com>
2019-07-03 12:57:55 -0400

    applied PEP formatting

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -11,7 +11,7 @@ class Node:
         self.right = None
         self.left = None
 
-class BinarySearchTree:
+class Binary_search_tree:
     def __init__(self):
         self.root = None
 
@@ -61,38 +61,38 @@ class BinarySearchTree:
 
     def _height(self, curr_node, height):
         if curr_node != None:
-            return max (self._height (curr_node.left, height), self._height(curr_node.right, height))+1
+            return max(self._height(curr_node.left, height), self._height(curr_node.right, height))+1
         else:
             return height
 
     # Search: searches if an element is in the tree
     # Complexity: O(log n)
-    def findTarget(self, value):
+    def find_target(self, value):
         if self.root != None:
-            return self._findTarget (self.root, value)
+            return self._find_target(self.root, value)
         else:
             return False
 
-    def _findTarget(self, curr_node, target):
+    def _find_target(self, curr_node, target):
         if curr_node != None:
             if target == curr_node.value:
                 return True
             elif (target > curr_node.value):
-                return self._findTarget(curr_node.right, target)
+                return self._find_target(curr_node.right, target)
             else:
-                return self._findTarget(curr_node.left, target)
+                return self._find_target(curr_node.left, target)
         else:
             return False
 
 # Testing the class
-tree = BinarySearchTree ()
+tree = Binary_search_tree ()
 tree.insert(10)
 tree.insert(5)
 tree.insert(6)
 tree.insert(4)
 tree.insert(11)
 tree.print()
-print ("Height =", tree.height())
-print ("Target found = ", tree.findTarget(15))
+print("Height =", tree.height())
+print("Target found = ", tree.find_target(15))
 
 
