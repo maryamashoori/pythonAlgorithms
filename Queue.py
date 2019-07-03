42fd3bab974c0a1910f737b73ea187baacc7b74e
Maryam Ashoori <maryam@us.ibm.com>
2019-07-03 12:29:01 -0400

    adding the complexity analysis for each function

--- Queue.py
+++ Queue.py
@@ -1,4 +1,5 @@
-#   BinarySearchTree.py
+#   Implementation of a queue
+#   https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
 #   PythonAlgorithms
 #
 #   Created by Maryam Ashoori on June 2019.
@@ -7,11 +8,14 @@ class Queue:
     def __init__(self):
         self.queue = []
 
-    # Push onto the queue. enqueue. add to the rear of the queue
-    def push(self, value):
+    # Add an element to the rear of the queue
+    # complexity: O(1)
+    def insert(self, value):
         self.queue.append(value)
 
-    def printq(self):
+    # Print the elements in the queue
+    # complexity: O(n)
+    def print(self):
         for item in self.queue:
             print(item)
 
@@ -23,17 +27,18 @@ class Queue:
     def front(self):
         return self.queue[0]
 
-    # Pop put of queue. dequeue. remove from the front
-    def pop(self):
-        front= self.front()
+    # Delete an element from the front of the queue
+    # complexity: O(1)
+    def delete(self):
+        front = self.front()
         self.queue.pop(0)
         return front
 
 if __name__ == "__main__":
     q = Queue()
-    q.push (5)
-    q.push (10)
-    q.printq()
-    print("queue:", q) #calling __repr__
-    print ("The item at the front of the queue is", q.front())
-    print (q.pop(), "is popped")
+    q.insert(5)
+    q.insert(10)
+    q.print()
+    print("queue:", q) # calling __repr__
+    print("The item at the front of the queue is", q.front())
+    print(q.delete(), "is popped")
