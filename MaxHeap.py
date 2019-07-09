ddccc99df8938b3e1137ae86e575e63c2813f640
Maryam Ashoori <maryam@us.ibm.com>
2019-07-09 11:12:06 -0400

    initial commit

---
+++ MaxHeap.py
@@ -0,0 +1,56 @@
+
+
+#   A Python implementation of Max Heap
+#   https://en.wikipedia.org/wiki/Heapsort
+#
+#   Created by Maryam Ashoori on July 2019.
+#   Tested in Python 3.7
+
+
+class Max_heap:
+    # Building a max heap
+    # Complexity: O(n)
+    def __init__(self, input):
+        self.heap_size = len(input)
+        self.elements = input
+        for i in range(int(self.heap_size/2-1), -1, -1):
+            self.max_heapify(i)
+
+    # Performance complexity: O(log n)
+    def max_heapify(self, index):
+        left = index * 2 + 1
+        right = index * 2 + 2
+        if left < self.heap_size and self.elements[left] > self.elements[index]:
+            largest = left
+        else:
+            largest = index
+
+        if right < self.heap_size and self.elements[right] > self.elements[largest]:
+            largest = right
+
+        # swaps the index element with the largest element
+        if largest != index:
+            temp = self.elements[index]
+            self.elements[index] = self.elements[largest]
+            self.elements[largest] = temp
+            self.max_heapify(largest)
+
+    # prints the root, swaps the root with the last element of the heap and heapifies the heap.
+    # Then it decreases the heap size
+    # Performance complexity: O(n log n) -- n times calling max_heapify function
+    def heap_sort(self):
+        while self.heap_size > 0:
+            print(self.elements[0])
+            self.elements[0] = self.elements[self.heap_size-1]
+            self.heap_size -= 1
+            self.max_heapify(0)
+
+    def __repr__(self):
+        # return ",".join(map(lambda x: str(x), self.queue))
+        return str(self.elements)
+
+if __name__ == "__main__":
+    myheap = Max_heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
+    print (myheap)
+    myheap.heap_sort()
+
---
+++ PriorityQueue.py
@@ -0,0 +1,74 @@
+
+
+#   A Python implementation of Priority Queue using MaxHeap
+#   https://en.wikipedia.org/wiki/Priority_queue
+#
+#   Created by Maryam Ashoori on July 2019.
+#   Tested in Python 3.7
+
+from MaxHeap import Max_heap
+
+class Priority_queue(Max_heap):
+
+    # returns the front element of the queue
+    # Performance complexity: O(1)
+    def heap_maximum(self):
+        if self.heap_size >0:
+            return self.elements[0]
+        else:
+            print("Error: Heap underflow)")
+            return -1
+
+    # extracts the front element of the queue
+    # Performance complexity: O(log n)
+    def heap_extract_max (self):
+        if self.heap_size >0:
+            max = self.elements[0]
+            self.elements[0] = self.elements[self.heap_size-1]
+            self.heap_size -=1
+            self.elements.pop(self.heap_size)
+            self.max_heapify(0)
+            return max
+        else:
+            print("Error: Heap underflow")
+            return -1
+
+    # implements an increase-key operation in a p queue
+    # Performance complexity: O(log n) since the path traced from index to the root has length of O(lg n)
+    def heap_increase_key(self, index, key):
+        if self.heap_size < index:
+            print("Error: Index out of range")
+        elif key < self.elements[index]:
+            print("Error: new key should be larger than the current key")
+        else:
+            self.elements[index] = key
+            #while the element in the heap is larger than it's parent, swap the two elements
+            while self.elements[index] > self.elements [int(index/2)]:
+                temp = self.elements [int(index/2)]
+                self.elements[int(index/2)] = self.elements[index]
+                self.elements[index] = temp
+                index = int(index/2)
+
+
+    # implements insert operation in a p queue . It takes a key and inserts at the right place in the p queue
+    # Performance complexity: similar to heap_increase_key : O(log n)
+    def max_heap_insert(self, key):
+        self.heap_size += 1
+        self.elements.append(-1000000)
+        self.heap_increase_key(self.heap_size-1, key)
+
+
+
+if __name__ == "__main__":
+    p_queue = Priority_queue([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
+    print(f"Priority queue: {p_queue}")
+    print(f"Front of the queue: {p_queue.heap_maximum()}")
+    p_queue.heap_increase_key(9, 15)
+    print(f"Priority queue after the value increase: {p_queue}")
+    p_queue.max_heap_insert(20)
+    print(f"New element inserted: {p_queue}")
+    p_queue.heap_extract_max()
+    print(f"Front element of the queue is removed: {p_queue}")
+
+
+
