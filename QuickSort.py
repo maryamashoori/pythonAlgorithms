afd851c11b12f9733e51b1c62664aa77c1d3d921
Maryam Ashoori <maryam@us.ibm.com>
2019-07-09 12:48:08 -0400

    added randomized quick sort

--- QuickSort.py
+++ QuickSort.py
@@ -15,14 +15,14 @@ class Quick_sort:
     def quick_sort(self, start, end):
         if start < end:
             pivot = self.partition(start, end)
-            self.quick_sort(0, pivot-1)
+            self.quick_sort(start, pivot-1)
             self.quick_sort(pivot+1, end)
 
     # Performance complexity: O(n)
     def partition(self, start, end):
         i = start -1
         pivot = self.elements[end -1]
-        for j in range (start, end):
+        for j in range(start, end):
             if self.elements[j] < pivot:
                 i +=1
                 # elements before i are smaller than pivot and elements
@@ -37,9 +37,32 @@ class Quick_sort:
         self.elements[end-1] = temp
         return i
 
+    # picks a random pivot to avoid the worst case performance
+    # Performance complexity: O(n log n)
+    def randomized_quick_sort(self, start, end):
+        if start < end:
+            pivot = self.randomized_partition(start, end)
+            self.randomized_quick_sort(start, pivot-1)
+            self.randomized_quick_sort(pivot+1, end)
+
+    def randomized_partition(self, start, end):
+        import random
+        random_index = random.randint(start, end-1)
+        # Swap the random index with the end element
+        temp = self.elements[end-1]
+        self.elements[end-1] = self.elements[random_index]
+        self.elements[random_index] = temp
+        return self.partition(start, end)
+
 if __name__ == "__main__":
     input = [2, 8, 7, 1, 3, 5, 6, 4]
     qsort = Quick_sort(input)
     print(f"input before sorting: {qsort.elements}")
     qsort.quick_sort(0, len(input))
     print(f"input after sorting: {qsort.elements}")
+
+    #Randomized Quick sort
+    rqsort = Quick_sort(input)
+    print(f"input before sorting: {rqsort.elements}")
+    rqsort.randomized_quick_sort(0, len(input))
+    print(f"input after sorting: {rqsort.elements}")
